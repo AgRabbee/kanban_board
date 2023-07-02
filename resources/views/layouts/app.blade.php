d824797d28474271f6ee2491a95caa737f0244c7
Md. Abdul Goni Rabbee <rabbee@batworld.com>
2023-07-02 21:19:33 +0600

    readme file updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,13 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="https://www.svgrepo.com/show/379476/kanban-board.svg">
 
     <title>Kanban Board</title>
     <style>
         body{
             background: #dfdfdf;
             font-family: "Candara";
+        }
+        .list-group-item{
+            cursor: pointer;
         }
     </style>
 </head>
